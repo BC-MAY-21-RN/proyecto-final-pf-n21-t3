89cf237b45636fe38bbb9e39d83021f0640efcdf
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-01-26 18:58:00 -0600

    Minor fixes.
    Co-authored-by: Dylanson25 <Dvillarreal0@ucol.mx>
    Co-authored-by: GerardoIbarra <libarra2@ucol.mx>
    Co-authored-by: Ernesto2899 <eramirez28@ucol.mx>

--- src/assets/styled.js
+++ src/assets/styled.js
@@ -1,23 +1,20 @@
 import styled from 'styled-components/native';
-
 export const Container = styled.View`
   flex-direction: column;
   flex: 1;
   padding: 6%;
-  background-color: #8231FF;
-  `;
-  
-  export const Boton = styled.TouchableOpacity`
+  background-color: #8231ff;
+`;
+export const Boton = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
-  border-radius: 50px;
+  border-radius: 18px;
   width: 100%;
   height: 50px;
-  background-color: #FFFFFF;
+  border-color: ${props => props.BColor || 'black'};
+  border: solid 2px;
+  background-color: ${props => props.BackColor || '#FFFFFF'};
   margin: 2% 0% 2% 0%;
   padding: 3px;
   font-weight: bold;
-  `;
-
-
-
+`;
