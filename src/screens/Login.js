b8e5c8eafd8d4944b3fcfd53380ce6f08c69ec14
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-01-26 18:49:34 -0600

    Minor styled components Fixes
    Co-authored-by: Dylanson25 <Dvillarreal0@ucol.mx>
    Co-authored-by: GerardoIbarra <libarra2@ucol.mx>
    Co-authored-by: Ernesto2899 <eramirez28@ucol.mx>

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,12 +9,13 @@ export const Login = () => {
     <Container>
       <Text style={{color:'black'}}>Estoy en Login</Text>
       
-      <TouchableOpacity onPress={() => {navigation.navigate('Main')}}>
-        <Text style={{color:'blue'}}>Ir a Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {navigation.navigate('Register')}}>
-        <Text style={{color:'blue'}}>Ir a Register</Text>
-      </TouchableOpacity>
+      <Boton onPress={() => {navigation.navigate('Main')}}>
+        <Text style={{color:'black'}}>Iniciar sesion</Text>
+      </Boton>
+
+      <Boton BackColor={'#4B367C'} BColor={'white'} onPress={() => {navigation.navigate('Register')}}>
+        <Text style={{color:'black'}}>Registrarse</Text>
+      </Boton>
     </Container>
   );
 };
