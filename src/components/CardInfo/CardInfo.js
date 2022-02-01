664544e38521292e6bfd1c6bf4f6dbe9ada1d2fa
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-02-01 11:06:13 -0600

    Prueba cardInfo

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CardContainer,RankingNumber,TrackTitle,TrackImage } from './Styled';
 
-export const CardInfo = () => {
+export const CardInfo = ({props}) => {
   return (
-    <SafeAreaView style={{paddingHorizontal: 23}}>
-    <CardContainer>
-      <RankingNumber>#1</RankingNumber>
-      <TrackImage /> 
-      <TrackTitle> Disculpa los malos pensamientos - PXNDX </TrackTitle>
-    </CardContainer>
-    </SafeAreaView>
+      <CardContainer>
+        <RankingNumber>#{props.id} </RankingNumber>
+      
+        <TrackImage  source ={{uri: props.album}} /> 
+      
+        <TrackTitle> {props.Titulo} - {props.Artista} </TrackTitle>
+    
+      </CardContainer>
   );
 };
 
--- src/components/CardInfo/Styled.js
+++ src/components/CardInfo/Styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const CardContainer = styled.View`
   display: flex;
@@ -7,6 +8,7 @@ export const CardContainer = styled.View`
   border: 1px solid black;
   height: 90px;
   padding: 10px 10px 10px 10px;
+  margin: 10px 0px 0px 0px;
   width: 100%;
   background-color: #C8A6FF;
   align-items: center;
@@ -25,10 +27,14 @@ color: black;
 font-size: 24px;
 `
 
-export const TrackImage = styled.Image.attrs(props =>({
-  source: require('../../assets/img/TrackCover.png'),
-}) )`
+export const TrackImage = styled.Image`
 width: 20%;
 height: 100%;
 `
-
+export const ContCard = styled(SafeAreaView)`
+background: #FFF064;
+padding: 7px;
+border: solid 2px black;
+width: 100%;
+height: 60%;
+`
