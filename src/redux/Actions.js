20c55960936aff73a6addc5d882f1e4481d61811
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-02-02 17:23:30 -0600

    REDUX SETTED
    Redux is currently working.
    -Create actions, reducer and the store.
    Actions:
    -setEmail
    -setName
    -setToken
    
    Co-authored-by: Dylanson25 <Dvillarreal0@ucol.mx>
    Co-authored-by: GerardoIbarra <libarra2@ucol.mx>
    Co-authored-by: Ernesto2899 <eramirez28@ucol.mx>

--- src/redux/Actions.js
+++ src/redux/Actions.js
@@ -1,6 +1,5 @@
 export const SET_NAME = 'SET_NAME';
 export const SET_EMAIL = 'SET_EMAIL';
-export const SET_PASSWORD = 'SET_PASSWORD';
 export const SET_TOKEN = 'SET_TOKEN';
 
 export const setName = (name) => ({
@@ -10,10 +9,6 @@ export const setName = (name) => ({
 export const setEmail = (email) => ({
   type: SET_EMAIL,
   payload: email,
-});
-export const setPassword = (password) => ({
-  type: SET_PASSWORD,
-  payload: password,
 });
 export const setToken = (token) => ({
   type: SET_TOKEN,
--- src/redux/Reducers.js
+++ src/redux/Reducers.js
@@ -1,4 +1,4 @@
-import {SET_NAME, SET_PASSWORD, SET_EMAIL, SET_TOKEN} from './Actions'
+import {SET_NAME, SET_EMAIL, SET_TOKEN} from './Actions'
 
 const initialState = {
   name : '',
@@ -11,8 +11,6 @@ function userData (state = initialState, action) {
   switch(action.type) {
     case SET_NAME:
       return {...state, name: action.payload}
-    case SET_PASSWORD:
-      return {...state, password: action.payload}
     case SET_EMAIL:
       return{...state, email: action.payload}
     case SET_TOKEN:
