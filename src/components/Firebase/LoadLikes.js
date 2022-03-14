d72e8d741f8fd84636d3f6046a05d04bae39949e
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-03-14 14:12:05 -0600

    Minor fixes

---
+++ src/components/Firebase/LoadLikes.js
@@ -0,0 +1,4 @@
+import { firebase } from "@react-native-firebase/auth";
+import { firestore } from "@react-native-firebase/firestore";
+import { Store } from "../../redux/Store";
+
--- src/screens/Library.js
+++ src/screens/Library.js
@@ -1,18 +1,15 @@
-import {Container} from '../assets/styled.js';
-import { Text, Button } from 'react-native';
+import {Title} from '../components/index';
+import {ScrlVw,Container} from '../assets/styled' 
 import { useNavigation } from '@react-navigation/native';
-
+import {ListPlay} from '../components/PlayInfo/ListPlay';
 import React from 'react';
 
 export const Library = () => {
   const navigation = useNavigation();
   return (
     <Container>
-      <Text style={{color: 'black'}}>Estoy en LIBRARY</Text>
-      <Button onPress={() => navigation.navigate('Tracks')} title='Ir a Tracks'/>
-      <Button onPress={() => navigation.navigate('Liked')} title='Ir a Liked'/>
-      <Button onPress={() => navigation.navigate('PlayList')} title='Ir a playlist'/>
-      <Button onPress={() => navigation.navigate('Player')} title='Ir a Player'/>
+      <Title Titulo={'Mis me gusta'}/>
+      <ListPlay />
     </Container>
   );
 };
